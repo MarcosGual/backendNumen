c477be13d0758cd962230fc303719fc7272730f6
MarcosGual <marcos.gual@outlook.com>
2022-09-18 23:05:40 -0300

    métodos get agregados

--- backendClase3/app.js
+++ backendClase3/app.js
@@ -1,1 +1,38 @@
-console.log("node funciona")
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo!");
+});
+
+app.listen(port, () => {
+  console.log(
+    `App de ejemplo escuchando en el puerto: http://localhost:${port}`
+  );
+});
+
+app.get("/usuario", (req, res) => {
+  res.send("Santiago Fernández");
+});
+
+app.get("/test", (req, res) => {
+  res.send({ msj: "Mensaje para el usuario", cantMsj: 2 });
+});
+
+app.get("/alumno/:nombre/:edad", (req, res) => {
+  //res.send(`Buenos días, alumno ${req.params.nombre}!`);
+  res.json({
+    alumno: req.params.nombre,
+    edad: req.params.edad,
+  });
+});
+
+app.get("/producto/tipo/:tipo/marca/:marca/modelo/:modelo", (req, res) => {
+    //res.send(`Buenos días, alumno ${req.params.nombre}!`);
+    res.json({
+      tipo: req.params.tipo,
+      marca: req.params.marca,
+      modelo: req.params.modelo
+    });
+  });
