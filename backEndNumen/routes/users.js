bb03059600e218917b16a38aa3c63e582f78cd6e
MarcosGual <marcos.gual@outlook.com>
2022-10-02 19:47:00 -0300

    proyecto back desde cero

--- backEndNumen/routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const { obtenerUsuarios } = require('../controllers/usersController');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/lista', obtenerUsuarios );
-
-module.exports = router;
