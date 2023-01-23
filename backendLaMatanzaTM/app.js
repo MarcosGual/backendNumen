5ac1f297448d102ea2217195b16ee937ed362021
MarcosGual <marcos.gual@outlook.com>
2023-01-23 12:05:02 -0300

    clase 7 lm-tm

--- backendLaMatanzaTM/app.js
+++ backendLaMatanzaTM/app.js
@@ -1,9 +1,13 @@
+//librerías
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require("express-session");
 const connect = require("./database/db");
-
+const cors = require("cors");
+//rutas
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
@@ -12,8 +16,16 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/usuarios", usersRouter);
--- backendLaMatanzaTM/routes/index.js
+++ backendLaMatanzaTM/routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const indexController = require("../controllers/indexController");
+const { route } = require("./users");
+const { check } = require("express-validator");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.get("/nuevaSesion", indexController.crearSesion);
+router.get("/verSesion", indexController.verSession);
+router.get("/cerrarSesion", indexController.cerrarSession);
+
+router.get("/verCookie", indexController.verCookie);
+router.get("/eliminarCookie", indexController.eliminarCookie);
+
+router.post(
+  "/login",
+  [
+    check("username")
+      .not()
+      .isEmpty()
+      .withMessage("El nombre de usuario es obligatorio"),
+    check("password").not().isEmpty().withMessage("El password es obligatorio"),
+  ],
+  indexController.logIn
+);
+router.get("/logout", indexController.logOut);
 
 module.exports = router;
